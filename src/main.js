84ee88712b0707302b1130842fea79001bee66e2
Eka Abashidze <eka.abashidze@redberry.ge>
2023-06-04 19:08:20 +0400

    refactor: create router.js file and extract routes there

--- src/main.js
+++ src/main.js
@@ -1,46 +1,11 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import StartPage from './pages/StartPage.vue';
-import FirstPage from './pages/FirstPage.vue';
-import SecondPage from './pages/SecondPage.vue';
-import ThirdPage from './pages/ThirdPage.vue';
-import FourthPage from './pages/FourthPage.vue';
 import './assets/fonts/bpg_arial_2009.ttf';
 import store from './store/index.js';
+import router from './router.js';
 
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'StartPage',
-      component: StartPage,
-    },
-    {
-      path: '/identification',
-      name: 'identification',
-      component: FirstPage,
-    },
-    {
-      path: '/questionnaire',
-      name: 'questionnaire',
-      component: SecondPage,
-    },
-    {
-      path: '/vaccine',
-      name: 'vaccine',
-      component: ThirdPage,
-    },
-    {
-      path: '/suggestions',
-      name: 'suggestions',
-      component: FourthPage,
-    }
-  ]
-});
 
 const app = createApp(App);
 
---
+++ src/router.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import StartPage from './pages/StartPage.vue';
+import FirstPage from './pages/FirstPage.vue';
+import SecondPage from './pages/SecondPage.vue';
+import ThirdPage from './pages/ThirdPage.vue';
+import FourthPage from './pages/FourthPage.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'StartPage',
+      component: StartPage,
+    },
+    {
+      path: '/identification',
+      name: 'identification',
+      component: FirstPage,
+    },
+    {
+      path: '/questionnaire',
+      name: 'questionnaire',
+      component: SecondPage,
+    },
+    {
+      path: '/vaccine',
+      name: 'vaccine',
+      component: ThirdPage,
+    },
+    {
+      path: '/suggestions',
+      name: 'suggestions',
+      component: FourthPage,
+    }
+  ]
+});
+
+export default router;

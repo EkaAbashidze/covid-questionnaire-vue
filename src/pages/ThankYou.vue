5162f94a999b016fe2ae6857dcb7ebb820504310
Eka Abashidze <eka.abashidze@redberry.ge>
2023-06-27 17:53:15 +0400

    feat/style: add thank you component and animation

---
+++ src/pages/ThankYou.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="bg-gray-900 flex items-center justify-center h-screen relative">
+    <transition class="fade-in">
+      <img
+        v-show="showThankYou"
+        src="../../public/images/thankyou.png"
+        alt="Thank You"
+      />
+    </transition>
+    <transition class="fade-in">
+      <div v-show="showStars" class="flex space-x-2 items-center">
+        <img
+          src="../../public/images/star.png"
+          alt="Star"
+          class="absolute top-[40%] left-[40%]"
+        />
+        <img
+          src="../../public/images/star.png"
+          alt="Star"
+          class="absolute top-[55%] left-[60%] w-[33px]"
+        />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showThankYou: true,
+      showStars: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showStars = true;
+    }, 2000);
+  },
+};
+</script>
+
+<style>
+.fade-in {
+  animation: fade-in 5s ease-in;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>

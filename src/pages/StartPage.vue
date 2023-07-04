8aad311fc4ddaa0eca544d9875ee20e51f07e94a
Eka Abashidze <eka.abashidze@redberry.ge>
2023-07-04 16:05:43 +0400

    fix: change the animation for the starting page

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="bg-white flex items-center justify-center h-screen">
-    <div class="parent">
-      <transition name="svg-transition">
-        <img
-          v-show="!isSmall"
-          src="/images/redberry.svg"
-          alt="Redberry Logo"
-          class="image"
-        />
-      </transition>
+    <transition name="">
+      <img
+        v-show="!isSmall"
+        src="/images/redberry.svg"
+        alt="Redberry Logo"
+        class="image"
+      />
+    </transition>
+    <div class="flex flex-col justify-center parent relative">
       <img
         v-show="!animationStatus"
         src="/images/redberry.svg"
         alt="Redberry Logo"
       />
-      <router-link v-if="isSmall" to="/identification"
-        >კითხვარის დაწყება</router-link
+      <router-link
+        v-show="showText"
+        to="/identification"
+        class="text fade-in text-[30px] font-bold flex text-center absolute transform translate-y-[200px]"
+        >კითხვარის
+        <br />
+        დაწყება</router-link
       >
     </div>
   </div>
@@ -27,15 +32,17 @@ export default {
     return {
       isSmall: false,
       animationStatus: true,
+      showText: false,
     };
   },
   mounted() {
-    setTimeout(() => {
-      this.isSmall = true;
-    }, 100);
+    this.isSmall = true;
     setTimeout(() => {
       this.animationStatus = false;
-    }, 3200);
+    }, 3000);
+    setTimeout(() => {
+      this.showText = true;
+    }, 3500);
   },
 };
 </script>  
@@ -48,20 +55,32 @@ export default {
   justify-content: center;
   align-items: center;
 }
+
 .image {
   width: 3000px;
   height: 3000px;
+  animation: scale-down 3s ease-in;
 }
-.svg-transition-enter-active,
-.svg-transition-leave-active {
-  transition: all 3s ease-in;
+
+.text {
+  animation: fade-in 1s ease-in;
 }
-.svg-transition-enter {
-  width: 3000px;
-  height: 3000px;
+
+@keyframes scale-down {
+  0% {
+    transform: scale(2);
+  }
+  100% {
+    transform: scale(0.05);
+  }
 }
-.svg-transition-leave-to {
-  width: 100px;
-  height: 100px;
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
